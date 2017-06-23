431936a2054b98d3ee2ecd149b4eab7a60f9bf40
Nazar Lozynskyi <lozynskyi.nazar@gmail.com>
2017-06-23 07:22:58 +0300

    add homeworks

---
+++ Homeworks/src/com/edu/homework1/Questions.java
@@ -0,0 +1,20 @@
+package com.edu.homework1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Questions {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+				
+			System.out.println("What is your name?");
+			String name = br.readLine();
+			System.out.println("Where are you live, " + name + "?");
+			String address = br.readLine();
+			System.out.println("You live in " + address + ".");
+	}
+
+}
