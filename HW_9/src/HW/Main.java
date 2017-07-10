fd7fcbe14884b7b53722d1443be87f473d224c19
Evey <byoro777@gmail.com>
2017-07-10 23:20:17 +0300

    Home work 9  fixed

--- HW_9/src/HW/Main.java
+++ HW_9/src/HW/Main.java
@@ -1,41 +1,36 @@
 package HW;
+
 import java.util.Scanner;
 
 /**
  * Created by Evey on 09.07.2017.
  */
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        double a, b;
+		Scanner input = new Scanner(System.in);
+		double a, b;
 
-        Main m = new Main();
-        try {
-            System.out.println("1");
-            a = input.nextDouble();
-            System.out.println("2");
-            b = input.nextDouble();
-            m.div(a,b);
-            System.out.println("Not throw ArithmeticException and return Double.INFINITY");
-        } catch (Exception e){
-            System.out.println(e);            
-        }finally {
-        	input.close();
+		Main m = new Main();
+		try {
+			System.out.println("1");
+			a = input.nextDouble();
+			System.out.println("2");
+			b = input.nextDouble();
+			m.div(a, b);
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			input.close();
 		}
 
-    }
-    void div(double  a, double b)throws ArithmeticException { 	
-        try {
-            if (b != 0) {
-                System.out.println("Result of " + a + " divided by " + b + " is : " + a / b);
-            } else
-            {
-            	throw new ArithmeticException();
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Sorry Division By Zero Is Not Possible");
-        }
-     }
+	}
+
+	void div(double a, double b) {
+		if (b == 0 || a == 0) {
+			throw new ArithmeticException();
+		}
+		System.out.println("Result of " + a + " divided by " + b + " is : " + a / b);
+	}
 
 }
